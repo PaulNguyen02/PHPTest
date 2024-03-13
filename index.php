d1e25b2cbeef4e96da3132e090e0a21a817818f5
Phúc Nguyễn <105428358+PaulNguyen02@users.noreply.github.com>
2024-03-13 11:24:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
     //Là một bridge nối Database, BaseModel, Base Controller, biến controllername thành lớp controller tương ứng và action thành các hàm tương ứng
-    require '/Core/config.php';
-    require '/Model/BaseModel.php';
-    require '/Controller/BaseController.php';
+    require './Core/config.php';
+    require './Model/BaseModel.php';
+    require './Controller/BaseController.php';
     $controllerName = ucfirst((strtolower($_REQUEST['controller'])?? 'Error').'Controller');    
     $actionName = $_REQUEST['action'] ?? 'error404';
-    require "/Controller/".$controllerName.".php";
+    require "./Controller/".$controllerName.".php";
     $controllerObj = new $controllerName;
     $controllerObj->$actionName();
     
