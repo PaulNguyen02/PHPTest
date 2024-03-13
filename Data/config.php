8ca75285307341d3e9fcc821bfc32da6fd17cfe0
Phúc Nguyễn <105428358+PaulNguyen02@users.noreply.github.com>
2024-03-13 10:53:15 +0700

    Update config.php

--- Data/config.php
+++ Data/config.php
@@ -3,7 +3,7 @@
     {
         $server="gateway01.ap-southeast-1.prod.aws.tidbcloud.com";        //127.0.0
         $user="3TZcRxCtCrvrfgX.root";               
-        $pass="47zX1ciKfQyp4O7c";
+        $pass="K9tFcYrvm5t19nKz";
         $database="Motoserv";
         $conn = mysqli_connect($server, $user, $pass, $database);
     }
