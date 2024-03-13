b6c79981c1fce9d99ed32c7727d4f57a2a2e5773
Phúc Nguyễn <105428358+PaulNguyen02@users.noreply.github.com>
2024-03-13 10:09:34 +0700

    Update config.php

--- Data/config.php
+++ Data/config.php
@@ -1,10 +1,10 @@
 <?php
     try
     {
-        $server="localhost";        //127.0.0
-        $user="root";               
-        $pass="";
-        $database="MotoSer";
+        $server="gateway01.ap-southeast-1.prod.aws.tidbcloud.com";        //127.0.0
+        $user="3TZcRxCtCrvrfgX.root";               
+        $pass="47zX1ciKfQyp4O7c";
+        $database="Motoserv";
         $conn = mysqli_connect($server, $user, $pass, $database);
     }
     catch(Exception $e) 
