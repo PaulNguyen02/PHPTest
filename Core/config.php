b99ebcee4ec178bf26be9824e962c2d45af55a1b
Phúc Nguyễn <105428358+PaulNguyen02@users.noreply.github.com>
2024-03-13 11:31:34 +0700

    Update config.php

--- Core/config.php
+++ Core/config.php
@@ -1,15 +1,16 @@
 <?php
     class Config
     {
-        const server = 'localhost';        //127.0.0
-        const user='root';               
-        const pass='';
-        const database='motoserv';        
+        $server="gateway01.ap-southeast-1.prod.aws.tidbcloud.com";        //127.0.0
+        $user="3TZcRxCtCrvrfgX.root";               
+        $pass="K9tFcYrvm5t19nKz";
+        $database="Motoserv";
+        $port = 4000;     
         function connect()
         {
             try
             {
-                $connect = mysqli_connect(self::server, self::user, self::pass, self::database);
+                $connect = mysqli_connect($server, $user, $pass, $database, $port);
                 return $connect;
             }
             catch(Exception $e) 
