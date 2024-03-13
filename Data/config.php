aa47ad261c73a6900ba4c0253963426161923775
Phúc Nguyễn <105428358+PaulNguyen02@users.noreply.github.com>
2024-03-13 11:06:19 +0700

    Update config.php

--- Data/config.php
+++ Data/config.php
@@ -5,7 +5,8 @@
         $user="3TZcRxCtCrvrfgX.root";               
         $pass="K9tFcYrvm5t19nKz";
         $database="Motoserv";
-        $conn = mysqli_connect($server, $user, $pass, $database);
+        $port = 4000;
+        $conn = mysqli_connect($server, $user, $pass, $database, $port);
     }
     catch(Exception $e) 
     {
